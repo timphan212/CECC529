69229597a2c7904d5978ecbc20c36b69be688284
timphan212 <timphan212@gmail.com>
2016-09-27 01:09:36 -0700

    main features should be complete

--- SearchEngineProject/src/searchengineproject/SearchEngineProject.java
+++ SearchEngineProject/src/searchengineproject/SearchEngineProject.java
@@ -16,8 +16,11 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -266,30 +269,62 @@ public class SearchEngineProject {
         }
     }
     
+    /**
+     * Searches the index for the token and then returns a list of integers
+     * of which files contain the token
+     * @param token a word
+     * @return return a list of integers representing the files the token is
+     * located at
+     */
     private static ArrayList<Integer> searchToken(String token) {
         ArrayList<Integer> files = new ArrayList<>();
         
+        // stem the token and then get the list of postional postings for the
+        // token
         ArrayList<PositionalPosting> pospostList = index
                 .getPositionalPosting(PorterStemmer.processToken(token));
-        for(PositionalPosting pospost : pospostList) {
-            files.add(pospost.getDocID());
+        
+        // if the token exists then add document id to a list
+        if(pospostList != null) {
+            for(PositionalPosting pospost : pospostList) {
+                files.add(pospost.getDocID());
+            }
         }
         
         return files;
     }
     
+    /**
+     * Finds the intersection of two file lists
+     * @param fileList1 the first list of files
+     * @param fileList2 the second list of files
+     * @return a merged list of fileList1 and fileList2
+     */
     private static ArrayList<Integer> mergeFileLists
         (ArrayList<Integer> fileList1, ArrayList<Integer> fileList2) {
         ArrayList<Integer> mergedFiles = new ArrayList<>();
         int i = 0, j = 0;
         
+        if(fileList1.size() == 0) {
+            return fileList2;
+        }
+        if(fileList2.size() == 0) {
+            return fileList1;
+        }
+        // keep looping until one of the list reaches the end
         while(i < fileList1.size() && j < fileList2.size()) {
+            // if the document id for the first list is less than the second
+            // then increment the pointer for the first list
             if(fileList1.get(i) < fileList2.get(j)) {
                 i++;
             }
+            // if the document id for the first list is greater than the second
+            // then increment the pointer for the second list
             else if(fileList1.get(i) > fileList2.get(j)) {
                 j++;
             }
+            // else if the document ids match then add the file number to the
+            // list and increment both pointers
             else {
                 mergedFiles.add(fileList1.get(i));
                 i++;
@@ -300,20 +335,93 @@ public class SearchEngineProject {
         return mergedFiles;    
     }
     
+    /**
+     * Finds the list of files where the phrase query can be found
+     * @param fileList list of lists of positional postings for each token
+     * @return return a list of integers representing the files
+     */
     private static ArrayList<Integer> phraseMergeFileLists(
             ArrayList<ArrayList<PositionalPosting>> fileList) {
+        // array list containing the files where the phrase is
         ArrayList<Integer> phraseFiles = new ArrayList<>();
+        // the first token's list of positional posting
+        ArrayList<PositionalPosting> file = fileList.get(0);
+
+        if(file != null) {
+            // Loop through the list of positional posting for the first token
+            for(PositionalPosting posPost : file) {
+                // grab the document id of the positional posting
+                int docID = posPost.getDocID();
+                // grab the list of positions associated with the document
+                ArrayList<Integer> positions = posPost.getPositions();
+                // boolean to check if the phrase was found
+                boolean phraseFound = false;
+                // counter to check if the phrase found matches the number of 
+                // tokens
+                int counter = 1;
+
+                // loop through the other token's list of positional postings
+                for(int j = 1; j < fileList.size(); j++) {
+                    // loop through the positional postings of the other tokens
+                    if(fileList.get(j) != null) {
+                        for(PositionalPosting otherPosPost : fileList.get(j)) {
+                            // check if the document ids match the first token
+                            if(otherPosPost.getDocID() == docID) {
+                                // loop through the positions of the first token
+                                // for one of the positional posting
+                                for(Integer position : positions) {
+                                    // check if the other token's positions
+                                    // contains the position of the first 
+                                    // token + 1
+                                    if(otherPosPost.getPositions()
+                                            .contains(position + j)) {
+                                        // set the boolean to be true
+                                        phraseFound = true;
+                                        // increment the counter
+                                        counter++;
+                                        break;
+                                    }
+                                }
+                            }
+                            // break out of the loop if the document id
+                            // surpasses the first token's document id
+                            else if(otherPosPost.getDocID() > docID) {
+                                break;
+                            }
+                        }
+                        // break out of the loop to prevent searching through 
+                        // the next token's positional posting if it could not
+                        // find the current token
+                        if(phraseFound == false) {
+                            break;
+                        }
+                    }
+                }
+                // if the phrase boolean is true and the counter is equal to the
+                // length of the phrase query then add the file to the list
+                if(phraseFound == true && fileList.size() == counter) {
+                    phraseFiles.add(docID);
+                }
+            }
+        }
         
         return phraseFiles;
     }
-        
+    
+    /**
+     * Loops through the list of lists to merge them into one list
+     * @param fileList a list of lists of files
+     * @return a list of integers that is from merging all the lists
+     */
     private static ArrayList<Integer> orFileLists(
             ArrayList<ArrayList<Integer>> fileList) {
         ArrayList<Integer> orFiles = new ArrayList<>();
         
+        // if the file list is only size of 1 then just set orFiles equal to it
         if(fileList.size() == 1) {
             orFiles = fileList.get(0);
         }
+        // else loop through all the lists, remove duplicates, then add them
         else {
             for(ArrayList<Integer> files : fileList) {
                 orFiles.removeAll(files);
@@ -324,7 +432,12 @@ public class SearchEngineProject {
         return orFiles;
     }
     
+    /**
+     * Prints out the document name and the number of documents found
+     * @param files a list of integers representing the files
+     */
     private static void printFiles(ArrayList<Integer> files) {
+        // loop through the list of files
         for(Integer file : files) {
             System.out.println(fileNames.get(file));
         }
