d9c1cdf170d2b822de53a98d542e5fa45421b6d4
Timothy Phan <timphan212@gmail.com>
2016-10-13 15:55:05 -0700

    added disk index files, need to integrate

---
+++ SearchEngineProject/src/searchengineproject/DiskInvertedIndex.java
@@ -0,0 +1,210 @@
+package searchengineproject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class DiskInvertedIndex {
+
+    private String mPath;
+    private RandomAccessFile mVocabList;
+    private RandomAccessFile mPostings;
+    private long[] mVocabTable;
+    private ArrayList<String> fileNames = new ArrayList<>();
+
+    public DiskInvertedIndex(String path) throws IOException {
+        try {
+            mPath = path;
+            mVocabList = new RandomAccessFile(new File(path, "vocab.bin"), "r");
+            mPostings = new RandomAccessFile(new File(path, "postings.bin"), "r");
+            mVocabTable = readVocabTable(path);
+            fileNames = readAllFileNames(path);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+
+    private static ArrayList<PositionalPosting> readPostingsFromFile(RandomAccessFile postings,
+            long postingsPosition) {
+        try {
+            // seek to the position in the file where the postings start.
+            postings.seek(postingsPosition);
+
+            // read the 4 bytes for the document frequency
+            byte[] buffer = new byte[4];
+            postings.read(buffer, 0, buffer.length);
+            
+            // use ByteBuffer to convert the 4 bytes into an int.
+            int documentFrequency = ByteBuffer.wrap(buffer).getInt();
+            
+            int lastDocId = 0, lastPosId = 0;
+            ArrayList<PositionalPosting> posPostList = new ArrayList<>();
+            
+            for (int i = 0; i < documentFrequency; i++) {
+                // list of positions
+                ArrayList<Integer> positions = new ArrayList<>();
+
+                // read the document id
+                byte[] docBuffer = new byte[4];
+                postings.read(docBuffer, 0, docBuffer.length);
+                int documentID = ByteBuffer.wrap(docBuffer).getInt() + lastDocId;
+                lastDocId = documentID;
+                
+                // read the term frequency
+                byte[] tfBuffer = new byte[4];
+                postings.read(tfBuffer, 0, tfBuffer.length);
+                int termFrequency = ByteBuffer.wrap(tfBuffer).getInt();
+                
+                // read the positions for the term in the document
+                for (int j = 0; j < termFrequency; j++) {
+                    byte[] posBuffer = new byte[4];
+                    postings.read(posBuffer, 0, posBuffer.length);
+                    int pos = ByteBuffer.wrap(posBuffer).getInt() + lastPosId;
+                    lastPosId = pos;
+                    positions.add(pos);
+                }
+
+                // create the positional posting list
+                PositionalPosting posPost = new PositionalPosting(documentID, positions);
+                posPostList.add(posPost);
+            }
+
+            return posPostList;
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+        return null;
+    }
+
+    public ArrayList<PositionalPosting> GetPostings(String term) {
+        long postingsPosition = binarySearchVocabulary(term);
+
+        if (postingsPosition >= 0) {
+            return readPostingsFromFile(mPostings, postingsPosition);
+        }
+
+        return null;
+    }
+
+    private long binarySearchVocabulary(String term) {
+        // do a binary search over the vocabulary, using the vocabTable and the file vocabList.
+        int i = 0, j = mVocabTable.length / 2 - 1;
+        while (i <= j) {
+            try {
+                int m = (i + j) / 2;
+                long vListPosition = mVocabTable[m * 2];
+                int termLength;
+                if (m == mVocabTable.length / 2 - 1) {
+                    termLength = (int) (mVocabList.length() - mVocabTable[m * 2]);
+                } else {
+                    termLength = (int) (mVocabTable[(m + 1) * 2] - vListPosition);
+                }
+
+                mVocabList.seek(vListPosition);
+
+                byte[] buffer = new byte[termLength];
+                mVocabList.read(buffer, 0, termLength);
+                String fileTerm = new String(buffer, "ASCII");
+
+                int compareValue = term.compareTo(fileTerm);
+                if (compareValue == 0) {
+                    // found it!
+                    return mVocabTable[m * 2 + 1];
+                } else if (compareValue < 0) {
+                    j = m - 1;
+                } else {
+                    i = m + 1;
+                }
+            } catch (IOException ex) {
+                System.out.println(ex.toString());
+            }
+        }
+        return -1;
+    }
+
+    private static long[] readVocabTable(String indexName) {
+        try {
+            long[] vocabTable;
+
+            RandomAccessFile tableFile = new RandomAccessFile(
+                    new File(indexName, "vocabTable.bin"),
+                    "r");
+
+            byte[] byteBuffer = new byte[4];
+            tableFile.read(byteBuffer, 0, byteBuffer.length);
+
+            int tableIndex = 0;
+            vocabTable = new long[ByteBuffer.wrap(byteBuffer).getInt() * 2];
+            byteBuffer = new byte[8];
+
+            while (tableFile.read(byteBuffer, 0, byteBuffer.length) > 0) { // while we keep reading 4 bytes
+                vocabTable[tableIndex] = ByteBuffer.wrap(byteBuffer).getLong();
+                tableIndex++;
+            }
+            tableFile.close();
+            return vocabTable;
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.toString());
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+        return null;
+    }
+
+    public int getTermCount() {
+        return mVocabTable.length / 2;
+    }
+
+    private static ArrayList<String> readAllFileNames(String path) throws IOException {
+        ArrayList<String> names = new ArrayList<>();
+        final Path currentWorkingPath = Paths.get(path).toAbsolutePath();
+        
+        Files.walkFileTree(currentWorkingPath, new SimpleFileVisitor<Path>() {
+            int mDocumentID = 0;
+
+            public FileVisitResult preVisitDirectory(Path dir,
+                    BasicFileAttributes attrs) {
+                // make sure we only process the current working directory
+                if (currentWorkingPath.equals(dir)) {
+                    return FileVisitResult.CONTINUE;
+                }
+                return FileVisitResult.SKIP_SUBTREE;
+            }
+
+            public FileVisitResult visitFile(Path file,
+                    BasicFileAttributes attrs) {
+                // only process .txt files
+                if (file.toString().endsWith(".json")) {
+                    // we have found a .json file; add its name to the fileName
+                    // list, then index the file and increase the document ID
+                    // counter.
+                    names.add(file.getFileName().toString());
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            // don't throw exceptions if files are locked/other errors occur
+            public FileVisitResult visitFileFailed(Path file,
+                    IOException e) {
+
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
+        
+        return names;
+    }
+    
+    public String getFileNames(int docID) {
+        return fileNames.get(docID);
+    }
+}
---
+++ SearchEngineProject/src/searchengineproject/IndexWriter.java
@@ -0,0 +1,249 @@
+package searchengineproject;
+
+import com.google.gson.Gson;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Writes an inverted indexing of a directory to disk.
+ */
+public class IndexWriter {
+
+    private String mFolderPath;
+
+    /**
+     * Constructs an IndexWriter object which is prepared to index the given
+     * folder.
+     */
+    public IndexWriter(String folderPath) {
+        mFolderPath = folderPath;
+    }
+
+    /**
+     * Builds and writes an inverted index to disk. Creates three files:
+     * vocab.bin, containing the vocabulary of the corpus; postings.bin,
+     * containing the postings list of document IDs; vocabTable.bin, containing
+     * a table that maps vocab terms to postings locations
+     */
+    public void buildIndex() {
+        buildIndexForDirectory(mFolderPath);
+    }
+
+    /**
+     * Builds the normal NaiveInvertedIndex for the folder.
+     */
+    private static void buildIndexForDirectory(String folder) {
+        PositionalInvertedIndex index = new PositionalInvertedIndex();
+
+        // Index the directory using a naive index
+        indexFiles(folder, index);
+
+        // at this point, "index" contains the in-memory inverted index 
+        // now we save the index to disk, building three files: the postings index,
+        // the vocabulary list, and the vocabulary table.
+        // the array of terms
+        String[] dictionary = index.getTerms();
+        // an array of positions in the vocabulary file
+        long[] vocabPositions = new long[dictionary.length];
+
+        buildVocabFile(folder, dictionary, vocabPositions);
+        buildPostingsFile(folder, index, dictionary, vocabPositions);
+    }
+
+    /**
+     * Builds the postings.bin file for the indexed directory, using the given
+     * NaiveInvertedIndex of that directory.
+     */
+    private static void buildPostingsFile(String folder, PositionalInvertedIndex index,
+            String[] dictionary, long[] vocabPositions) {
+        FileOutputStream postingsFile = null;
+        try {
+            postingsFile = new FileOutputStream(
+                    new File(folder, "postings.bin")
+            );
+
+            // simultaneously build the vocabulary table on disk, mapping a term index to a
+            // file location in the postings file.
+            FileOutputStream vocabTable = new FileOutputStream(
+                    new File(folder, "vocabTable.bin")
+            );
+
+            // the first thing we must write to the vocabTable file is the number of vocab terms.
+            byte[] tSize = ByteBuffer.allocate(4)
+                    .putInt(dictionary.length).array();
+            vocabTable.write(tSize, 0, tSize.length);
+            int vocabI = 0;
+            for (String s : dictionary) {
+                // for each String in dictionary, retrieve its postings.
+                ArrayList<PositionalPosting> postings = index.getPositionalPosting(s);
+
+                // write the vocab table entry for this term: the byte location of the term in the vocab list file,
+                // and the byte location of the postings for the term in the postings file.
+                byte[] vPositionBytes = ByteBuffer.allocate(8)
+                        .putLong(vocabPositions[vocabI]).array();
+                vocabTable.write(vPositionBytes, 0, vPositionBytes.length);
+
+                byte[] pPositionBytes = ByteBuffer.allocate(8)
+                        .putLong(postingsFile.getChannel().position()).array();
+                vocabTable.write(pPositionBytes, 0, pPositionBytes.length);
+
+                // write the postings file for this term. first, the document frequency for the term, then
+                // the document IDs, encoded as gaps.
+                byte[] docFreqBytes = ByteBuffer.allocate(4)
+                        .putInt(postings.size()).array();
+                postingsFile.write(docFreqBytes, 0, docFreqBytes.length);
+                
+                int lastDocId = 0, lastPosId = 0;
+                
+                for(PositionalPosting post : postings) {
+                    // write the document id
+                    byte[] docIdBytes = ByteBuffer.allocate(4)
+                            .putInt(post.getDocID() - lastDocId).array();
+                    postingsFile.write(docIdBytes, 0, docIdBytes.length);
+                    lastDocId = post.getDocID();
+                    // write term frequency for document
+                    byte[] termFreqBytes = ByteBuffer.allocate(4)
+                        .putInt(post.getPositions().size()).array();
+                    postingsFile.write(termFreqBytes, 0, termFreqBytes.length);
+                    // write each position of term
+                    for(Integer position : post.getPositions()) {
+                        byte[] posIdBytes = ByteBuffer.allocate(4)
+                                .putInt(position - lastPosId).array();
+                        postingsFile.write(posIdBytes, 0, posIdBytes.length);
+                        lastPosId = position;
+                    }
+                }
+                
+                vocabI++;
+            }
+            vocabTable.close();
+            postingsFile.close();
+        } catch (FileNotFoundException ex) {
+        } catch (IOException ex) {
+        } finally {
+            try {
+                postingsFile.close();
+            } catch (IOException ex) {
+            }
+        }
+    }
+
+    private static void buildVocabFile(String folder, String[] dictionary,
+            long[] vocabPositions) {
+        OutputStreamWriter vocabList = null;
+        try {
+         // first build the vocabulary list: a file of each vocab word concatenated together.
+            // also build an array associating each term with its byte location in this file.
+            int vocabI = 0;
+            vocabList = new OutputStreamWriter(
+                    new FileOutputStream(new File(folder, "vocab.bin")), "ASCII"
+            );
+
+            int vocabPos = 0;
+            for (String vocabWord : dictionary) {
+                // for each String in dictionary, save the byte position where that term will start in the vocab file.
+                vocabPositions[vocabI] = vocabPos;
+                vocabList.write(vocabWord); // then write the String
+                vocabI++;
+                vocabPos += vocabWord.length();
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.toString());
+        } catch (UnsupportedEncodingException ex) {
+            System.out.println(ex.toString());
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        } finally {
+            try {
+                vocabList.close();
+            } catch (IOException ex) {
+                System.out.println(ex.toString());
+            }
+        }
+    }
+
+    private static void indexFiles(String folder, final PositionalInvertedIndex index) {
+        final Path currentWorkingPath = Paths.get(folder).toAbsolutePath();
+
+        try {
+            Files.walkFileTree(currentWorkingPath, new SimpleFileVisitor<Path>() {
+                int mDocumentID = 0;
+
+                public FileVisitResult preVisitDirectory(Path dir,
+                        BasicFileAttributes attrs) {
+                    // make sure we only process the current working directory
+                    if (currentWorkingPath.equals(dir)) {
+                        return FileVisitResult.CONTINUE;
+                    }
+                    return FileVisitResult.SKIP_SUBTREE;
+                }
+
+                public FileVisitResult visitFile(Path file,
+                        BasicFileAttributes attrs) {
+                    // only process .txt files
+                    if (file.toString().endsWith(".json")) {
+                  // we have found a .txt file; add its name to the fileName list,
+                        // then index the file and increase the document ID counter.
+                        // System.out.println("Indexing file " + file.getFileName());
+                        indexFile(file.toFile(), index, mDocumentID);
+                        mDocumentID++;
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                // don't throw exceptions if files are locked/other errors occur
+                public FileVisitResult visitFileFailed(Path file,
+                        IOException e) {
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+            });
+        } catch (IOException ex) {
+            Logger.getLogger(IndexWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static void indexFile(File fileName, PositionalInvertedIndex index,
+            int documentID) {
+
+        try {
+            Gson gson = new Gson();
+            NPSDocument npsDoc = gson.fromJson(new FileReader(fileName),
+                    NPSDocument.class);
+            SimpleTokenStream stream = new SimpleTokenStream(npsDoc.body);
+            String term = stream.nextToken();
+            int counter = 0;
+            while(term != null) {
+                
+                if (term.contains("-")) {
+                    index.addTerm(PorterStemmer.processToken(term
+                            .replace("-", "")), documentID, counter);
+                    String[] hyphenWord = term.split("-");
+
+                    for (String word : hyphenWord) {
+                        index.addTerm(PorterStemmer.processToken(word),
+                                documentID, counter);
+                    }
+                }
+                else {
+                    index.addTerm(PorterStemmer.processToken(term),
+                            documentID, counter);
+                }
+                term = stream.nextToken();
+                counter++;
+            }
+        } catch (Exception ex) {
+            System.out.println("Index File " + ex.toString());
+        }
+    }
+}
